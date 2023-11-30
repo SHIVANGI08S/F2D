e38aea086bfbe978fb0d0d59c2d350b5cb187313
Shivangi Singh <shivangi.singh08110@gmail.com>
2023-11-30 11:09:47 +0530

    updated

---
+++ F2D-Frontend/src/components/BuyerContent/Buyer_Cart.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import "../../style/Buyer_Content.css";
+import { Link } from "react-router-dom";
+
+const Buyer_Cart = () => {
+  return (
+    <ChakraProvider>
+      <Box display="flex">
+        <Box
+          display="flex"
+          flexDirection="column"
+          color="#665039"
+          className="buyer_content"
+          paddingLeft="10"
+          paddingTop="20"
+        >
+          <Box className="buyer_content_options" padding="5" paddingTop="10" >
+            <Link to="/BuyerContent">
+              <i class="fa-solid fa-house"></i> Home
+            </Link>
+          </Box>
+          <Box className="buyer_content_options" padding="5" color="#e8c897">
+          <Link to="/BuyerCart">
+          <i class="fa-solid fa-cart-shopping"></i>  Cart
+          </Link>
+          
+          </Box>
+          <Box className="buyer_content_options" padding="5">
+          <Link to="/BuyerAccount">
+          <i class="fa-solid fa-user"></i>  Account
+          </Link>
+          
+          </Box>
+        </Box>
+        <Box></Box>
+      </Box>
+    </ChakraProvider>
+  )
+}
+
+export default Buyer_Cart
--- F2D-Frontend/src/components/FarmerContent/ProductList.js
+++ F2D-Frontend/src/components/FarmerContent/ProductList.js
@@ -1,28 +1,16 @@
-import React, { useState , useContext, useEffect} from 'react';
+import React, { useState } from "react";
 import { Box, Button, ChakraProvider } from "@chakra-ui/react";
-import productContext from '../../Contexts/smallProducts/productContext';
 
 const ProductList = () => {
-  
+  const [products, setProducts] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [product, setProduct] = useState({
-    name: '',
-    price: '',
-    image: '',
-    quantity: '',
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+    quantity: "",
   });
-
-  
-  const context = useContext(productContext);
-  const { products, addProduct , deleteProduct, getAllProducts} = context;
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      getAllProducts();
-    } else {
-    }
-  }, [getAllProducts]);
-
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const openModal = () => {
     setShowModal(true);
@@ -33,70 +21,127 @@ const ProductList = () => {
   };
 
   const handleChange = (e) => {
-    setProduct({ ...product, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setNewProduct({
+      ...newProduct,
+      [name]: value,
+    });
   };
 
-  const add = () => {
-        addProduct(product.name, product.price, product.image, product.quantity);
-  
-        // Clear the fields immediately after adding an idea
-       setProduct({
-          name: '',
-          price: '',
-          image: '',
-          quantity: '',
-        });
+  const addProduct = () => {
+    setProducts([...products, newProduct]);
+    setNewProduct({ name: "", price: "", image: "", quantity: "" });
     closeModal();
   };
+
+  const deleteProduct = (index) => {
+    const updatedProducts = [...products];
+    updatedProducts.splice(index, 1);
+    setProducts(updatedProducts);
+  };
+
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
-  
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <ChakraProvider>
       <div>
         <input
-          className='search_button'
+          className="search_button"
           type="text"
           placeholder="Search by Name"
           value={searchTerm}
           onChange={handleSearch}
         />
-        <button className='add_button' onClick={openModal}><i class="fa-solid fa-plus"></i> Add new Product</button>
+        <button className="add_button" onClick={openModal}>
+          <i class="fa-solid fa-plus"></i> Add new Product
+        </button>
       </div>
 
       {showModal && (
         <Box className="modal">
-          <Box className="modal-content" width="80" bg="#b18e64" color="#665039" fontFamily="serif" fontSize="xl">
+          <Box
+            className="modal-content"
+            width="80"
+            bg="#b18e64"
+            color="#665039"
+            fontFamily="serif"
+            fontSize="xl"
+          >
             <span className="close" onClick={closeModal}>
               &times;
             </span>
             <label>Name:</label>
-            <input className='input_box' type="text" name="name" value={product.name} onChange={handleChange}  />
+            <input
+              className="input_box"
+              type="text"
+              name="name"
+              value={newProduct.name}
+              onChange={handleChange}
+            />
             <label>Price:</label>
-            <input className='input_box'  type="text" name="price" value={product.price} onChange={handleChange} />
+            <input
+              className="input_box"
+              type="text"
+              name="price"
+              value={newProduct.price}
+              onChange={handleChange}
+            />
             <label>Image URL:</label>
-            <input className='input_box'  type="text" name="image" value={product.image} onChange={handleChange} />
+            <input
+              className="input_box"
+              type="text"
+              name="image"
+              value={newProduct.image}
+              onChange={handleChange}
+            />
             <label>Quantity in Stock:</label>
-            <input className='input_box'  type="text" name="quantity" value={product.quantity} onChange={handleChange} />
-            <button onClick={add}>Add</button>
+            <input
+              className="input_box"
+              type="text"
+              name="quantity"
+              value={newProduct.quantity}
+              onChange={handleChange}
+            />
+            <button onClick={addProduct}>Add</button>
           </Box>
         </Box>
       )}
 
       <Box className="product-list" display="flex" flexWrap="wrap">
-        {products.map((product, index) => (
-          <Box key={index} className="product-card" margin="5" bg="#E8C897" color="#665039" display="flex">
-<Box paddingTop="2">
-<img src={product.image} alt={product.name} className='product_img'/>
-</Box>
-            <Box padding="2"  >
-            <h3>{product.name}</h3>
-            <p>Price: {product.price} Rs</p>
-            <p>{product.quantity} kgs in stock</p>
-            <Button color="#665039" bg="#E8C897"  onClick={() => deleteProduct(product._id)}><i class="fa-solid fa-trash fa-lg"></i></Button>
+        {filteredProducts.map((product, index) => (
+          <Box
+            key={index}
+            className="product-card"
+            margin="5"
+            bg="#E8C897"
+            color="#665039"
+            display="flex"
+          >
+            <Box paddingTop="2">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product_img"
+              />
             </Box>
-        
+            <Box padding="2">
+              <h3>{product.name}</h3>
+              <p>Price: {product.price} Rs</p>
+              <p>{product.quantity} kgs in stock</p>
+              <Button
+                color="#665039"
+                bg="#E8C897"
+                onClick={() => deleteProduct(index)}
+              >
+                <i class="fa-solid fa-trash fa-lg"></i>
+              </Button>
+            </Box>
           </Box>
         ))}
       </Box>
